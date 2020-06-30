d2b2e873c83c5674aaf8c1f7910cc0ef0eb7f812
Marcelsuarez <captain.falcon.cool@gmail.com>
2020-06-30 17:01:09 -0500

    Changing to .vue structure, simple componenets added

---
+++ src/components/SubTitle.vue
@@ -0,0 +1,27 @@
+<template>
+
+<div class="subNameDiv">
+    <subNameTitle>Current Subreddit</subNameTitle>
+</div>
+
+</template>
+
+
+<script>
+export default {
+  name: 'SubTitle'
+}
+</script>
+
+
+<style scoped>
+.subNameDiv {
+    text-align: right;
+    padding: 0px 25px;
+}
+
+subNameTitle {
+    font-size: 32px;
+}
+
+</style>
